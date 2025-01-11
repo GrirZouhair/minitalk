ce079118091afc7f41971d28425aee0a48c22ab7
Zouhair Grir <zogrir@e3r8p15.1337.ma>
2025-01-11 15:35:01 +0100

    update the server file

--- mandatory/server.c
+++ mandatory/server.c
@@ -6,7 +6,7 @@
 /*   By: zogrir <zogrir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:21:07 by zogrir            #+#    #+#             */
-/*   Updated: 2025/01/11 11:45:40 by zogrir           ###   ########.fr       */
+/*   Updated: 2025/01/11 15:31:00 by zogrir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,36 +24,38 @@ ft_putstr_fd("\n", 1);
 ft_putstr_fd("	Process Id (PID) : ", 1);
 ft_putnbr(getpid());
 ft_putchar('\n');
-
 }
 
 void signlHandler(int sig, siginfo_t *info, void *just)
 {
     static int bit_count = 0;
     static char received_char = 0;
-    
-    // Use info to access the PID of the sending process
-    pid_t sender_pid = info->si_pid;
-    
-    
-    received_char <<= 1;
-    
-    // Set the least significant bit depending on the signal type
-    if (sig == SIGUSR1)
-        received_char |= 1;  // Set bit to 1 if SIGUSR1 is received
-    
-    bit_count++;
+    static int pid;
 
-    if (bit_count == 8)
-    {
-        if (received_char == 0)
-            write(1, "\n", 1);
-        else
-            write(1, &received_char, 1);
-        
-        bit_count = 0;
-        received_char = 0;
-    }
+	(void)just;
+	pid = info->si_pid;
+	if(pid != info->si_pid)
+	{
+		bit_count = 0;
+		received_char = 0;
+	}
+	//received_char <<= 1;
+    //received_char = received_char << 1 | (sig - SIGUSR1);
+	if(sig== SIGUSR1)
+		received_char = received_char << 1 | 1;
+	else
+		received_char = received_char << 1 | 0;
+	bit_count++;
+	while (bit_count == 8)
+	{
+		if (received_char == 0)
+			write(1, "\n", 1);
+		else
+			write(1, &received_char, 1);
+			
+		bit_count = 0;
+		received_char = 0;
+	}
 }
 
 int main(int ac, char **av)
